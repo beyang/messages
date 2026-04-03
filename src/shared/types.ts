511ca50cabc8dac20103ece9ef869c54d0254270
Beyang Liu <beyang.liu@gmail.com>
2026-04-03 12:16:16 -0700

    zod schema for all core types

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+type AssertTrue<T extends true> = T;
+
 export interface Message {
   id: string;
   sourceURL: string;
@@ -25,8 +27,6 @@ export const messageSchema: z.ZodType<Message> = z
     }
     return message;
   });
-
-type AssertTrue<T extends true> = T;
 type _messageSatisfiesMessageSchema = AssertTrue<
   Message extends z.input<typeof messageSchema> ? true : false
 >;
@@ -40,6 +40,19 @@ export interface Convo {
   messages: Message[];
 }
 
+export const convoSchema: z.ZodType<Convo> = z.object({
+  id: z.string(),
+  sourceURL: z.string(),
+  messages: z.array(messageSchema),
+});
+
+type _convoSatisfiesConvoSchema = AssertTrue<
+  Convo extends z.input<typeof convoSchema> ? true : false
+>;
+type _convoSchemaProducesConvo = AssertTrue<
+  z.output<typeof convoSchema> extends Convo ? true : false
+>;
+
 export type JsonSerializable =
   | string
   | number
@@ -48,11 +61,35 @@ export type JsonSerializable =
   | JsonSerializable[]
   | { [key: string]: JsonSerializable };
 
+export const jsonSerializableSchema: z.ZodType<JsonSerializable> = z.json();
+
+type _jsonSerializableSatisfiesJsonSerializableSchema = AssertTrue<
+  JsonSerializable extends z.input<typeof jsonSerializableSchema> ? true : false
+>;
+type _jsonSerializableSchemaProducesJsonSerializable = AssertTrue<
+  z.output<typeof jsonSerializableSchema> extends JsonSerializable
+    ? true
+    : false
+>;
+
 export interface ProviderConfig<A extends JsonSerializable = JsonSerializable> {
   id: string;
   type: string;
   args: A;
 }
+
+export const providerConfigSchema: z.ZodType<ProviderConfig> = z.object({
+  id: z.string(),
+  type: z.string(),
+  args: jsonSerializableSchema,
+});
+
+type _providerConfigSatisfiesProviderConfigSchema = AssertTrue<
+  ProviderConfig extends z.input<typeof providerConfigSchema> ? true : false
+>;
+type _providerConfigSchemaProducesProviderConfig = AssertTrue<
+  z.output<typeof providerConfigSchema> extends ProviderConfig ? true : false
+>;
 
 export interface Inbox {
   id: string;
@@ -60,17 +97,73 @@ export interface Inbox {
   providers: ProviderConfig[];
 }
 
+export const inboxSchema: z.ZodType<Inbox> = z.object({
+  id: z.string(),
+  convos: z.array(convoSchema),
+  providers: z.array(providerConfigSchema),
+});
+
+type _inboxSatisfiesInboxSchema = AssertTrue<
+  Inbox extends z.input<typeof inboxSchema> ? true : false
+>;
+type _inboxSchemaProducesInbox = AssertTrue<
+  z.output<typeof inboxSchema> extends Inbox ? true : false
+>;
+
 export type FetchConvosResult = {
   convos: Convo[];
   needsAuth?: { url: string };
   errors?: string[];
 };
 
+export const fetchConvosResultSchema: z.ZodType<FetchConvosResult> = z.object({
+  convos: z.array(convoSchema),
+  needsAuth: z
+    .object({
+      url: z.string(),
+    })
+    .optional(),
+  errors: z.array(z.string()).optional(),
+});
+
+type _fetchConvosResultSatisfiesFetchConvosResultSchema = AssertTrue<
+  FetchConvosResult extends z.input<typeof fetchConvosResultSchema>
+    ? true
+    : false
+>;
+type _fetchConvosResultSchemaProducesFetchConvosResult = AssertTrue<
+  z.output<typeof fetchConvosResultSchema> extends FetchConvosResult
+    ? true
+    : false
+>;
+
 export interface SecretStore {
   get(key: string): string | null;
   set(key: string, value: string): void;
   delete(key: string): void;
 }
+
+export const secretStoreSchema: z.ZodType<SecretStore> = z.object({
+  get: z.function({
+    input: [z.string()],
+    output: z.string().nullable(),
+  }),
+  set: z.function({
+    input: [z.string(), z.string()],
+    output: z.void(),
+  }),
+  delete: z.function({
+    input: [z.string()],
+    output: z.void(),
+  }),
+});
+
+type _secretStoreSatisfiesSecretStoreSchema = AssertTrue<
+  SecretStore extends z.input<typeof secretStoreSchema> ? true : false
+>;
+type _secretStoreSchemaProducesSecretStore = AssertTrue<
+  z.output<typeof secretStoreSchema> extends SecretStore ? true : false
+>;
 
 export interface Provider<A extends JsonSerializable = JsonSerializable> {
   type: string;
@@ -79,3 +172,31 @@ export interface Provider<A extends JsonSerializable = JsonSerializable> {
   authInitURL?(args: A, baseURL: string): string;
   handleAuthCallback?(secret: string, secrets: SecretStore): void;
 }
+
+export const providerSchema: z.ZodType<Provider> = z.object({
+  type: z.string(),
+  id: z.string(),
+  fetchConvos: z.function({
+    input: [jsonSerializableSchema, secretStoreSchema],
+    output: z.promise(fetchConvosResultSchema),
+  }),
+  authInitURL: z
+    .function({
+      input: [jsonSerializableSchema, z.string()],
+      output: z.string(),
+    })
+    .optional(),
+  handleAuthCallback: z
+    .function({
+      input: [z.string(), secretStoreSchema],
+      output: z.void(),
+    })
+    .optional(),
+});
+
+type _providerSatisfiesProviderSchema = AssertTrue<
+  Provider extends z.input<typeof providerSchema> ? true : false
+>;
+type _providerSchemaProducesProvider = AssertTrue<
+  z.output<typeof providerSchema> extends Provider ? true : false
+>;
