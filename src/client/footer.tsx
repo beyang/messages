5142ef236f535b9804d2167802c532b1cc9cf215
Beyang Liu <beyang.liu@gmail.com>
2026-04-11 22:17:39 -0700

    make footer smaller

--- src/client/footer.tsx
+++ src/client/footer.tsx
@@ -10,14 +10,7 @@ export function Footer({ status, height }: FooterProps) {
   const safeStatus = sanitizeForTerminalText(status);
 
   return (
-    <Box
-      flexDirection="column"
-      borderStyle="single"
-      borderColor="white"
-      paddingX={1}
-      height={height}
-      overflow="hidden"
-    >
+    <Box flexDirection="column" paddingX={1} height={height} overflow="hidden">
       <Text wrap="truncate-end">{safeStatus}</Text>
     </Box>
   );
